df8eb914740ea569b7c8de0f87fccfa2432f305b
MuhammadBurhan235 <121776239+MuhammadBurhan235@users.noreply.github.com>
2024-06-04 21:21:33 +0700

    Update App.java
    mengubah variable, dengan penyesuaian

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,15 +5,15 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGING = "Hello World!";
 
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
+        System.out.println(MESSAGING);
     }
 
     public String getMessage() {
-        return MESSAGE;
+        return MESSAGING;
     }
 }
